9310a39c753836a4014a61988d4290a8e58c0f86
Giulia <giulia.albertini3@studio.unibo.it>
2025-03-05 15:26:19 +0100

    add sample main

---
+++ src/main/java/it/unibo/oop/lastcrown/App.java
@@ -0,0 +1,11 @@
+package it.unibo.oop.lastcrown;
+
+public final class App {
+    private App() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void main(final String[] args) {
+
+    }
+}
