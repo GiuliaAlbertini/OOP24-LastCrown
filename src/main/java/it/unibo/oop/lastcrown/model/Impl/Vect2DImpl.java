b141e74e9520826282bfb4a1782fb2e5131c0fce
Giulia <giulia.albertini3@studio.unibo.it>
2025-03-26 16:15:41 +0100

    Add Vect2D interface and implementation

---
+++ src/main/java/it/unibo/oop/lastcrown/model/Impl/Vect2DImpl.java
@@ -0,0 +1,55 @@
+package it.unibo.oop.lastcrown.model.Impl;
+
+import it.unibo.oop.lastcrown.model.api.Vect2D;
+
+public class Vect2DImpl implements Vect2D {
+    private final double x;
+    private final double y;
+
+    public Vect2DImpl(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+    
+    @Override
+    public Vect2D sum(Vect2D v) {
+        return new Vect2DImpl(this.x + v.x(), this.y +v.y());
+    }
+
+    @Override
+    public Vect2D subtract(Vect2D v) {
+       return new Vect2DImpl(this.x - v.x(), this.y - v.y());
+    }
+
+    @Override
+    public Vect2D mul(final double factor) {
+        return new Vect2DImpl(this.x * factor, this.y * factor);
+    }
+
+    @Override
+    public Vect2D normalized() {
+        double module=(double)Math.sqrt(x*x+y*y);
+        return new Vect2DImpl(x/module,y/module);
+    }
+
+    @Override
+    public double module() {
+        return (double)Math.sqrt(x*x+y*y);
+    }
+
+    @Override
+    public double x() {
+        return x;
+    }
+
+    @Override
+    public double y() {
+        return y;
+    }
+    
+    @Override
+    public String toString(){
+        return "Vect2d("+x+", "+y+")";
+    }
+
+}
