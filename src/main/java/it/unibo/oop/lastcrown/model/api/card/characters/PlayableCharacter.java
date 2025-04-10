bfa05afb345b02e86442ca7600d7503a19e28c94
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-04-10 16:46:39 +0200

    Change PlayableCharacter inheritance and methods

--- src/main/java/it/unibo/oop/lastcrown/model/api/card/characters/PlayableCharacter.java
+++ src/main/java/it/unibo/oop/lastcrown/model/api/card/characters/PlayableCharacter.java
@@ -10,16 +10,6 @@ public interface PlayableCharacter extends GenericCharacter {
     String getType();
 
     /**
-     * @return total copies of this character playable in one round
-     */
-    int getCopiesPerRound();
-
-    /**
-     * @return amount of energy needed to play this character
-     */
-    int getEnergyToPlay();
-
-    /**
      * @return this character action range (distance form witch this character can spot enemies) 
      */
     int getActionRange();
