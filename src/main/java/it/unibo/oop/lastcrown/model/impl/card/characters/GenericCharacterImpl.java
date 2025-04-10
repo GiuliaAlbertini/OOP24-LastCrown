2b822912fc988a618ed321fee692776fb510c32b
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-04-10 17:36:31 +0200

    Change GenericCharacterImpl to follow new Inheritance structure

--- src/main/java/it/unibo/oop/lastcrown/model/impl/card/characters/GenericCharacterImpl.java
+++ src/main/java/it/unibo/oop/lastcrown/model/impl/card/characters/GenericCharacterImpl.java
@@ -1,13 +1,12 @@
 package it.unibo.oop.lastcrown.model.impl.card.characters;
 
-import it.unibo.oop.lastcrown.model.api.card.Requirement;
 import it.unibo.oop.lastcrown.model.api.card.characters.GenericCharacter;
-import it.unibo.oop.lastcrown.model.impl.card.CardImpl;
 
 /**
- * A standard implementation of Character interface.
+ * A standard implementation of GenericCharacter interface.
  */
-public class GenericCharacterImpl extends CardImpl implements GenericCharacter {
+public class GenericCharacterImpl implements GenericCharacter {
+    private final String name;
     private final int attack;
     private final int health;
     private final double atckRecoveryTime;
@@ -15,19 +14,23 @@ public class GenericCharacterImpl extends CardImpl implements GenericCharacter {
 
     /**
      * @param name the name of this character
-     * @param requirement the requirement to own this character(ex Coins, 200)
      * @param attack the attack value of this character
      * @param health the health value of this character
      * @param atckRecoveryTime the attack recovery time of this character(time lap between attacks)
      * @param speedMultiplier the speed multiplier of this character(ex 1.5 -> standard_speed * 1.5)
      */
-    public GenericCharacterImpl(final String name, final Requirement requirement,
-    final int attack, final int health, final double atckRecoveryTime, final double speedMultiplier) {
-        super(name, requirement);
+    public GenericCharacterImpl(final String name, final int attack,
+    final int health, final double atckRecoveryTime, final double speedMultiplier) {
+        this.name = name;
         this.attack = attack;
         this.health = health;
         this.atckRecoveryTime = atckRecoveryTime;
         this.speedMultiplier = speedMultiplier;
+    }
+
+    @Override
+    public final String getName() {
+        return this.name;
     }
 
     @Override
@@ -49,4 +52,5 @@ public class GenericCharacterImpl extends CardImpl implements GenericCharacter {
     public final double getSpeedMultiplier() {
         return this.speedMultiplier;
     }
+
 }
