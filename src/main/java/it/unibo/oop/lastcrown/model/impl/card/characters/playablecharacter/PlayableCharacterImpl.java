6946cadae7fe00342958e4a4f5f3b42eb833a48a
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-04-10 17:38:25 +0200

    Change PlayableCharacterImpl to follow new Inheritance structure

--- src/main/java/it/unibo/oop/lastcrown/model/impl/card/characters/playablecharacter/PlayableCharacterImpl.java
+++ src/main/java/it/unibo/oop/lastcrown/model/impl/card/characters/playablecharacter/PlayableCharacterImpl.java
@@ -1,6 +1,5 @@
 package it.unibo.oop.lastcrown.model.impl.card.characters.playablecharacter;
 
-import it.unibo.oop.lastcrown.model.api.card.Requirement;
 import it.unibo.oop.lastcrown.model.api.card.characters.PlayableCharacter;
 import it.unibo.oop.lastcrown.model.impl.card.characters.GenericCharacterImpl;
 
@@ -9,6 +8,7 @@ import it.unibo.oop.lastcrown.model.impl.card.characters.GenericCharacterImpl;
  */
 public class PlayableCharacterImpl extends GenericCharacterImpl implements PlayableCharacter {
     private final String type;
+    private final int cost;
     private final int copiesPerRound;
     private final int energyToPlay;
     private final int actionRange;
@@ -16,7 +16,7 @@ public class PlayableCharacterImpl extends GenericCharacterImpl implements Playa
     /**
      * @param name the name of this character
      * @param type can be "melee" or "ranged"
-     * @param requirement the requirement to own this character(ex Coins, 200)
+     * @param cost total amount of coins to spend to own this playable character
      * @param attack the attack value of this character
      * @param health the health value of this character
      * @param atckRecoveryTime the attack recovery time of this character(time lap between attacks)
@@ -25,11 +25,12 @@ public class PlayableCharacterImpl extends GenericCharacterImpl implements Playa
      * @param speedMultiplier the speed multiplier of this character(ex 1.5 -> standard_speed * 1.5)
      * @param actionRange this character action range (distance form witch this character can spot enemies)
      */
-    public PlayableCharacterImpl(final String name, final String type, final Requirement requirement,
+    public PlayableCharacterImpl(final String name, final String type, final int cost,
     final int attack, final int health, final double atckRecoveryTime, final int copiesPerRound,
     final int energyToPlay, final double speedMultiplier, final int actionRange) {
-        super(name, requirement, attack, health, atckRecoveryTime, speedMultiplier);
+        super(name, attack, health, atckRecoveryTime, speedMultiplier);
         this.type = type;
+        this.cost = cost;
         this.copiesPerRound = copiesPerRound;
         this.energyToPlay = energyToPlay;
         this.actionRange = actionRange;
@@ -40,8 +41,13 @@ public class PlayableCharacterImpl extends GenericCharacterImpl implements Playa
         return this.type;
     }
 
+     @Override
+    public final int getCost() {
+        return this.cost;
+    }
+
     @Override
-    public final int getCopiesPerRound() {
+    public final int getCopiesPerMatch() {
         return this.copiesPerRound;
     }
 
