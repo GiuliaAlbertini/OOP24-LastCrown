fcf632c3d9a68efe7f0f415cb51627c22e75da00
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-04-10 20:01:19 +0200

    Add Spell interface and SpellEffect

---
+++ src/main/java/it/unibo/oop/lastcrown/model/api/card/spell/SpellEffect.java
@@ -0,0 +1,14 @@
+package it.unibo.oop.lastcrown.model.api.card.spell;
+
+import java.util.Optional;
+
+/**
+ * A specific effect applied by a spell that changes characters statistic during the match.
+ * @param category the category of character statistic to which the effect is aimed (health, attack...).
+ * @param amount the amount of change to a specific character statistic caused by the effect (+200 health, +3 attack).
+ * @param target the target of the effect (friendly characters or enemies).
+ * @param duration the duration of the effect expressed in seconds. If it is missing,
+ * the effect is activated as soon as the spell is cast.
+ */
+public record SpellEffect(String category, int amount, String target, Optional<Integer> duration) {
+}
