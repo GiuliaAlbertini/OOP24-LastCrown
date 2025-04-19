faf04821d71992ae18088ee563db79bce11cf4ed
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-04-19 18:49:43 +0200

    Add InGameCharacterFactory

---
+++ src/main/java/it/unibo/oop/lastcrown/model/impl/card/characters/ingamecharacter/InGameCharacterFactory.java
@@ -0,0 +1,27 @@
+package it.unibo.oop.lastcrown.model.impl.card.characters.ingamecharacter;
+
+import it.unibo.oop.lastcrown.model.api.card.characters.InGameCharacter;
+
+/**
+ * Create a InGameCharacter with the specified parameters.
+ */
+public final class InGameCharacterFactory {
+    private InGameCharacterFactory() { }
+
+    /**
+     * @param type the type of this character (melee, ranged, hero, enemy)
+     * @param name the name of this character
+     * @param health the health value of this character
+     * @param attack the attack value of this character
+     * @param atckRecoveryTime the attack recovery time of this character(time lap between attacks)
+     * @param speedMultiplier the speed multiplier of this character
+     * @param inCombat True to consider this character in combat, False otherwise
+     * @return a new InGameCharacter
+     */
+    public static InGameCharacter createInGameCharacter(final String type,
+     final String name, final int health, final int attack,
+     final double atckRecoveryTime, final double speedMultiplier, final boolean inCombat) {
+        return new InGameCharacterImpl(type, name, health, attack,
+         atckRecoveryTime, speedMultiplier, inCombat);
+     }
+}
