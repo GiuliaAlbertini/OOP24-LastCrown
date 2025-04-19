c8816c8e4e88b871a49b9e4bff107d46e2eabd54
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-04-19 18:39:21 +0200

    Add InGameCharacter implementation

---
+++ src/main/java/it/unibo/oop/lastcrown/model/impl/card/characters/ingamecharacter/InGameCharacterImpl.java
@@ -0,0 +1,125 @@
+package it.unibo.oop.lastcrown.model.impl.card.characters.ingamecharacter;
+
+import it.unibo.oop.lastcrown.model.api.card.characters.InGameCharacter;
+
+/**
+ * A standard implementation of InGameCharacter interface.
+ */
+public class InGameCharacterImpl implements InGameCharacter {
+    private final String name;
+    private final String type;
+    private final double atckRecoveryTime;
+    private int currentHealth;
+    private int maximumHealth;
+    private int attack;
+    private double speedMultiplier;
+    private boolean inCombat;
+    private boolean dead;
+
+    /**
+     * @param type the type of this character (melee, ranged, hero, enemy)
+     * @param name the name of this character
+     * @param health the health value of this character
+     * @param attack the attack value of this character
+     * @param atckRecoveryTime the attack recovery time of this character
+     * @param speedMultiplier the speed multiplier of this character
+     * @param inCombat True to be considered in combat, False otherwise
+     */
+    public InGameCharacterImpl(final String type, final String name,
+     final int health, final int attack, final double atckRecoveryTime,
+     final double speedMultiplier, final boolean inCombat) {
+        this.type = type;
+        this.name = name;
+        this.currentHealth = health;
+        this.maximumHealth = health;
+        this.attack = attack;
+        this.atckRecoveryTime = atckRecoveryTime;
+        this.speedMultiplier = speedMultiplier;
+        this.inCombat = inCombat;
+        this.dead = false;
+    }
+
+    @Override
+    public final String getType() {
+        return this.type;
+    }
+
+    @Override
+    public final String getName() {
+        return this.name;
+    }
+
+    @Override
+    public final void takeDamage(final int damage) {
+        this.currentHealth = this.currentHealth - damage;
+        if (this.currentHealth <= 0) {
+            this.currentHealth = 0;
+            this.dead = true;
+        }
+    }
+
+    @Override
+    public final void restoreHealth(final int cure) {
+        this.currentHealth = this.currentHealth + cure;
+        if (this.currentHealth > this.maximumHealth) {
+            this.currentHealth = this.maximumHealth;
+        }
+    }
+
+    @Override
+    public final void changeMaximumHealth(final int variation) {
+        this.maximumHealth = this.maximumHealth + variation;
+        if (variation < 0) {
+            if (this.currentHealth > this.maximumHealth) {
+            this.currentHealth = this.maximumHealth;
+            }
+        } else {
+            this.currentHealth = this.currentHealth + variation;
+        }
+    }
+
+    @Override
+    public final int getHealthPercentage() {
+        return 100 * this.currentHealth / this.maximumHealth;
+    }
+
+    @Override
+    public final int getAttack() {
+       return this.attack;
+    }
+
+    @Override
+    public final double getAtckRecoveryTime() {
+        return this.atckRecoveryTime;
+    }
+
+    @Override
+    public final void changeAttack(final int variation) {
+        this.attack = this.attack + variation;
+    }
+
+    @Override
+    public final double getSpeedMultiplier() {
+        return this.speedMultiplier;
+    }
+
+    @Override
+    public final void changeSpeedMultiplier(final double variation) {
+        this.speedMultiplier = this.speedMultiplier + variation;
+    }
+
+    @Override
+    public final void setInCombat(final boolean inCombat) {
+        this.inCombat = inCombat;
+    }
+
+    @Override
+    public final boolean isInCombat() {
+        return this.inCombat;
+    }
+
+    @Override
+    public final boolean isDead() {
+       return this.dead;
+    }
+}
