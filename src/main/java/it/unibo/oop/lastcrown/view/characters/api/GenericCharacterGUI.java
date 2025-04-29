fb5bb3e0bcd5cab37906163dbbd575019b73ac88
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-04-29 21:34:38 +0200

    Add GenericCharacterGUI interface

---
+++ src/main/java/it/unibo/oop/lastcrown/view/characters/api/GenericCharacterGUI.java
@@ -0,0 +1,52 @@
+package it.unibo.oop.lastcrown.view.characters.api;
+
+/**
+ * An interface that handles the graphic side of a generic character.
+ */
+public interface GenericCharacterGUI {
+    /**
+     * Set the size of the animation panel linked to this character GUI.
+     * @param newWidth new panel width
+     * @param newHeight new panel height
+     */
+    void setAnimationPanelSize(int newWidth, int newHeight);
+
+    /**
+     * Set the position of the animation panel linked to this character GUI (centered on the coordinates given).
+     * @param x coordinate x
+     * @param y coordinate y
+     */
+    void setAnimationPanelPosition(int x, int y);
+
+    /**
+     * Set the animation speed multiplier.
+     * @param newSpeedMul new speed multiplier
+     */
+    void setSpeedMultiplier(double newSpeedMul);
+
+    /**
+     * Set the new health bar percent linked to this character.
+     * @param percentage
+     */
+    void setHealthPercentage(int percentage);
+
+    /**
+     * Make this character start running.
+     */
+    void startRunLoop();
+
+    /**
+     * Make this character stop.
+     */
+    void startStopLoop();
+
+    /**
+     * Make this character start attacking.
+     */
+    void startAttackLoop();
+
+    /**
+     * Make this character die.
+     */
+    void startDeathSequence();
+}
