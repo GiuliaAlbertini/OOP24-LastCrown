8b614fde871455bd8d9015aab010e766b8b2e1ce
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-04-30 15:07:21 +0200

    Add CharacterAnimationPanel interface and change the implementation in order to use it

--- src/main/java/it/unibo/oop/lastcrown/view/characters/impl/CharacterAnimationPanelImpl.java
+++ src/main/java/it/unibo/oop/lastcrown/view/characters/impl/CharacterAnimationPanelImpl.java
@@ -1,20 +1,23 @@
-package it.unibo.oop.lastcrown.view.characters;
+package it.unibo.oop.lastcrown.view.characters.impl;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 
 import javax.swing.JPanel;
 
+import it.unibo.oop.lastcrown.view.characters.CharacterHealthBar;
+import it.unibo.oop.lastcrown.view.characters.api.CharacterAnimationPanel;
+
 /**
  * A custom JPanel that contains the animation frames and the healthbar of a specific character.
  */
-final class CharacterAnimationPanel extends JPanel {
+final class CharacterAnimationPanelImpl extends JPanel implements CharacterAnimationPanel {
     private static final long serialVersionUID = 1L;
     private static final int BAR_HEIGHT_DIVISOR = 9;
     private CharacterHealthBar healthBar;
     private transient Image currentImage;
 
-    private CharacterAnimationPanel() { }
+    private CharacterAnimationPanelImpl() { }
 
     /**
      * Creates a new instance of CharacterAnimationPanel.
@@ -23,8 +26,8 @@ final class CharacterAnimationPanel extends JPanel {
      * @param color the color of the health bar associated with this panel
      * @return new CharacterAnimationPanel
      */
-    public static CharacterAnimationPanel create(final int width, final int height, final Color color) {
-        final CharacterAnimationPanel instance = new CharacterAnimationPanel();
+    public static CharacterAnimationPanelImpl create(final int width, final int height, final Color color) {
+        final CharacterAnimationPanelImpl instance = new CharacterAnimationPanelImpl();
         instance.init(width, height, color);
         return instance;
     }
@@ -35,35 +38,26 @@ final class CharacterAnimationPanel extends JPanel {
         this.setSize(width, height);
         this.add(this.healthBar);
     }
-    /**
-     * Set the next frame to be shown.
-     * @param image the next frame to be shown
-     */
+
+    @Override
     public void setCharacterImage(final Image image) {
         this.currentImage = image;
         this.repaint();
     }
 
-    /**
-     * Realign this character health bar position after moving the animation panel.
-     */
+    @Override
     public void setHealthBarPosition() {
         this.healthBar.setBounds(0, this.getHeight() / BAR_HEIGHT_DIVISOR,
          this.healthBar.getWidth(), this.healthBar.getHeight());
         this.healthBar.setVisible(true);
     }
 
-    /**
-     * Set new percentage value of this character health bar.
-     * @param percentage
-     */
+    @Override
     public void setHealthBarImage(final int percentage) {
         this.healthBar.setPercentage(percentage);
     }
 
-    /**
-     * Clear this animation panel image and remove the health bar.
-     */
+    @Override
     public void disposeClosing() {
         this.setCharacterImage(null);
         this.remove(this.healthBar);
