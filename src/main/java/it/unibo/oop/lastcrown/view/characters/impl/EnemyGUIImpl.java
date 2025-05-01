b22c8cd176cf527f2410ca1c68fcae86ae31dbb1
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-05-01 09:53:15 +0200

    Add EnemyGUIImpl

---
+++ src/main/java/it/unibo/oop/lastcrown/view/characters/impl/EnemyGUIImpl.java
@@ -0,0 +1,42 @@
+package it.unibo.oop.lastcrown.view.characters.impl;
+
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+import it.unibo.oop.lastcrown.view.characters.Keyword;
+import it.unibo.oop.lastcrown.view.characters.api.CharacterAttackObserver;
+import it.unibo.oop.lastcrown.view.characters.api.EnemyGUI;
+
+/**
+ * A standard implementation of EnemyGUI.
+ */
+public class EnemyGUIImpl extends GenericCharacterGUIImpl implements EnemyGUI {
+    private final List<BufferedImage> runImages;
+    private final List<BufferedImage> retreatImages;
+
+    /**
+     * @param obs the observer of the enemy attacks
+     * @param charType the type of the enemy (normal enemy or boss)
+     * @param charName the name of the enemy
+     * @param speedMultiplier the speed multiplier of the enemy
+     * @param width the horizontal size of the character animation panel
+     * @param height the vertical size of the character animation panel
+     */
+    public EnemyGUIImpl(final CharacterAttackObserver obs, final String charType,
+     final String charName, final Double speedMultiplier, final int width, final int height) {
+        super(obs, charType, charName, speedMultiplier, width, height);
+
+        this.runImages = this.getSelectedFrames(Keyword.RUN_LEFT.get());
+        this.retreatImages = this.getSelectedFrames(Keyword.RETREAT.get());
+    }
+
+    @Override
+    public final void startRunLoop() {
+        this.startAnimationSequence(this.runImages, Keyword.RUN_LEFT);
+    }
+
+    @Override
+    public final void startRetreatLoop() {
+        this.startAnimationSequence(this.retreatImages, Keyword.RUN_RIGHT);
+    }
+}
