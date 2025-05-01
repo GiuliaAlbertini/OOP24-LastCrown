c2d305cc7ce78d8261a7dc0cafe312b73149cebd
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-05-01 22:17:29 +0200

    Change HeroController interface in order to follow the changes done to the HeroGUI interface and implementation

--- src/main/java/it/unibo/oop/lastcrown/controller/characters/api/HeroController.java
+++ src/main/java/it/unibo/oop/lastcrown/controller/characters/api/HeroController.java
@@ -1,29 +1,17 @@
 package it.unibo.oop.lastcrown.controller.characters.api;
 
-import javax.swing.JPanel;
-
 /**
  * A controller that handles the behaviour of the hero.
  */
 public interface HeroController extends GenericCharacterController {
 
     /**
-     * Set the panel where hero must appear and his new position (centered).
-     * @param x horizontal position of the hero animation panel
-     * @param y vertical position of the hero animation panel
-     * @param width width of the hero animation panel
-     * @param height height of the hero animation panel
-     * @param panel the new panel where hero must appear
-     */
-    void setCurrentPanel(int x, int y, int width, int height, JPanel panel);
-
-    /**
-     * Dispose the graphic components of this hero to be in the Match Panel.
+     * Dispose the graphic components of this hero to be in the Match View.
      */
     void setHeroInGame();
 
     /**
-     * Dispose the graphic components of this hero to be in the Match panel.
+     * Dispose the graphic components of this hero to be in the Shop View.
      */
     void setHeroInShop();
 
