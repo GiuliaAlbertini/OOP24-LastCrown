184a5917bbc679a2c963e6e3e18de1b6d69b75f7
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-05-02 19:40:06 +0200

    Add HeroContrller implementation and change interface

--- src/main/java/it/unibo/oop/lastcrown/controller/characters/api/HeroController.java
+++ src/main/java/it/unibo/oop/lastcrown/controller/characters/api/HeroController.java
@@ -1,5 +1,7 @@
 package it.unibo.oop.lastcrown.controller.characters.api;
 
+import javax.swing.JPanel;
+
 /**
  * A controller that handles the behaviour of the hero.
  */
@@ -7,13 +9,19 @@ public interface HeroController extends GenericCharacterController {
 
     /**
      * Dispose the graphic components of this hero to be in the Match View.
+     * @param matchPanel the panel where the match takes place
+     * @param initialX the initial horizontal coordinate of the animation panel(centered on the given value)
+     * @param initialY the initial vertical coordinate of the animation panel(centered on the given value)
      */
-    void setHeroInGame();
+    void setHeroInGame(JPanel matchPanel, int initialX, int initialY);
 
     /**
      * Dispose the graphic components of this hero to be in the Shop View.
+     * @param shopPanel the panel corresponding to the shop
+     * @param initialX the initial horizontal coordinate of the animation panel(centered on the given value)
+     * @param initialY the initial vertical coordinate of the animation panel(centered on the given value)
      */
-    void setHeroInShop();
+    void setHeroInShop(JPanel shopPanel, int initialX, int initialY);
 
     /**
      * Make this hero run to left.
