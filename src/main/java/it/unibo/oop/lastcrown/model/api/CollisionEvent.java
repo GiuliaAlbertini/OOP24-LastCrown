a3456d4d43103f80eef8fb36497f7bc5b0fc3cf2
Giulia <giulia.albertini3@studio.unibo.it>
2025-05-02 15:37:35 +0200

    Add CollisionEvent between two collidable entities

---
+++ src/main/java/it/unibo/oop/lastcrown/model/api/CollisionEvent.java
@@ -0,0 +1,17 @@
+package it.unibo.oop.lastcrown.model.api;
+
+/**
+ * Represents a collision event between two collidable entities.
+ */
+public interface CollisionEvent {
+
+    /**
+     * @return the first collidable object involved in the collision
+     */
+    Collidable getCollidable1();
+
+    /**
+     * @return the second collidable object involved in the collision
+     */
+    Collidable getCollidable2();
+}
