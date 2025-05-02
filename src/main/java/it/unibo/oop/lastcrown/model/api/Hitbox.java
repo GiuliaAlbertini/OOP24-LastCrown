d77c6ed0630c418d9ad26b18f8082ddde579fc82
Giulia <giulia.albertini3@studio.unibo.it>
2025-05-02 15:53:18 +0200

    Change Hitbox interface

--- src/main/java/it/unibo/oop/lastcrown/model/api/Hitbox.java
+++ src/main/java/it/unibo/oop/lastcrown/model/api/Hitbox.java
@@ -1,6 +1,36 @@
 package it.unibo.oop.lastcrown.model.api;
 
+/**
+ * Represents the hitbox of a game object, used for collision detection.
+ */
 public interface Hitbox {
+
+    /**
+     * Returns the position (top-left corner) of this hitbox.
+     *
+     * @return the position as a Point2D
+     */
     Point2D getPosition();
-    boolean checkCollision(Hitbox a, Hitbox b);
+
+    /**
+     * Returns the width of the hitbox.
+     *
+     * @return the width in pixels
+     */
+    int getWidth();
+
+    /**
+     * Returns the height of the hitbox.
+     *
+     * @return the height in pixels
+     */
+    int getHeight();
+
+    /**
+     * Checks whether this hitbox collides with another hitbox.
+     *
+     * @param other Hitbox to check collision against
+     * @return true if the two hitboxes collide, false otherwise
+     */
+    boolean checkCollision(Hitbox other);
 }
