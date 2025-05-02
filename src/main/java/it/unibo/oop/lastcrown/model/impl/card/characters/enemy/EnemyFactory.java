2c383349f018266dcb0f52e07a1fb094dc38bdfb
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-05-02 17:30:31 +0200

    Add EnemyFactory

---
+++ src/main/java/it/unibo/oop/lastcrown/model/impl/card/characters/enemy/EnemyFactory.java
@@ -0,0 +1,25 @@
+package it.unibo.oop.lastcrown.model.impl.card.characters.enemy;
+
+import it.unibo.oop.lastcrown.model.api.card.characters.Enemy;
+
+/**
+ * Creates a Enemy with the specified parameters.
+ */
+public final class EnemyFactory {
+
+    private EnemyFactory() { }
+
+    /**
+     * @param name the name of this enemy
+     * @param rank the rank of this enemy
+     * @param enemyType the type of this enemy (standard enemy or boss)
+     * @param attack the attack value of this character
+     * @param health the health value of this character
+     * @param speedMultiplier the speed multiplier of this enemy
+     * @return a new Enemy
+     */
+    public static Enemy createEnemy(final String name, final int rank, final String enemyType,
+    final int attack, final int health, final double speedMultiplier) {
+        return new EnemyImpl(name, rank, enemyType, attack, health, speedMultiplier);
+    }
+}
