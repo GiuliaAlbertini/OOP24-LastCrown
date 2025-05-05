bb0a72168a417c59da2859b21ea163949cb3a1ff
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-05-05 22:03:55 +0200

    Add interface Deck

---
+++ src/main/java/it/unibo/oop/lastcrown/model/user/api/Deck.java
@@ -0,0 +1,31 @@
+package it.unibo.oop.lastcrown.model.user.api;
+
+import java.util.Set;
+
+import it.unibo.oop.lastcrown.model.card.CardIdentifier;
+
+/**
+ * Provides a rappresentation of the deck of the user.
+ */
+public interface Deck {
+    /**
+     * Provides the deck as a {@link Set} of {@link CardIdentifier}.
+     * 
+     * @return the set of {@link CardIdentifier} in the deck
+     */
+    Set<CardIdentifier> getDeck();
+
+    /**
+     * Add the given card to the deck.
+     * 
+     * @param card the {@link CardIdentifier} to add
+     */
+    void addCard(CardIdentifier card);
+
+    /**
+     * Remove the given card from the deck.
+     * 
+     * @param card the {@link CardIdentifier} to remove
+     */
+    void removeCard(CardIdentifier card);
+}
