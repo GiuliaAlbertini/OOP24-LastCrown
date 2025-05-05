e0bffa76a6cced58db442e859d2d8bd2a0656602
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-05-05 18:46:48 +0200

    Add class SpellsParser

---
+++ src/main/java/it/unibo/oop/lastcrown/model/file_handling/impl/SpellsParser.java
@@ -0,0 +1,83 @@
+package it.unibo.oop.lastcrown.model.file_handling.impl;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import it.unibo.oop.lastcrown.model.card.CardIdentifier;
+import it.unibo.oop.lastcrown.model.file_handling.api.Parser;
+import it.unibo.oop.lastcrown.model.spell.api.Spell;
+import it.unibo.oop.lastcrown.model.spell.api.SpellEffect;
+import it.unibo.oop.lastcrown.model.spell.impl.SpellFactory;
+
+/**
+ * Implementation of a {@link Parser} for the {@link Spell} file.
+ */
+public class SpellsParser implements Parser<Map<CardIdentifier, Spell>> {
+
+    private static final String REGEX = "\\s*,\\s*";
+    private static final String NONE  = "none";
+    private static final String TYPE_SPELL = "spell";
+
+    /**
+     * Parses each string of {@code lines} into an {@link Entry} <{@link CardIdentifier}, {@link Spell}>.
+     * It returns a {@link Map} of those entries.
+     * Assumes that every line is in the correct format.
+     * 
+     * @param lines the lines to parse
+     * @return a map of the entries produced
+     */
+    @Override
+    public Map<CardIdentifier, Spell> parse(final List<String> lines) {
+        return lines.stream()
+            .map(String::trim)
+            .filter(line -> !line.isEmpty())
+            .map(this::createSpellEntry)
+            .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private Entry<CardIdentifier, Spell> createSpellEntry(final String line) {
+        final String[] tokens = line.split(REGEX);
+        int index = 0;
+        final int id = Integer.parseInt(tokens[index]);
+        index++;
+        final String name = tokens[index];
+        index++;
+        final int cost = Integer.parseInt(tokens[index]);
+        index++;
+        final int copiesPerRound = Integer.parseInt(tokens[index]);
+        index++;
+        final int energyToPlay = Integer.parseInt(tokens[index]);
+        index++;
+        final String effectCategory = tokens[index];
+        index++;
+        final int effectAmount = Integer.parseInt(tokens[index]);
+        index++;
+        final String effectTarget = tokens[index];
+        index++;
+        final Optional<Integer> duration = NONE.equalsIgnoreCase(tokens[index])
+            ? Optional.empty()
+            : Optional.of(Integer.parseInt(tokens[index]));
+        final SpellEffect effect = new SpellEffect(
+            effectCategory,
+            effectAmount,
+            effectTarget,
+            duration
+        );
+        index++;
+        final Optional<Integer> actionRange = NONE.equalsIgnoreCase(tokens[index])
+            ? Optional.empty()
+            : Optional.of(Integer.parseInt(tokens[index]));
+        final Spell spell = SpellFactory.createSpell(
+            name,
+            cost,
+            copiesPerRound,
+            energyToPlay,
+            effect,
+            actionRange
+        );
+        return Map.entry(new CardIdentifier(id, TYPE_SPELL), spell);
+    }
+}
