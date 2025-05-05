b07c3749903ce7a3860c5e02db9b440f883ccfe2
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-05-05 14:06:28 +0200

    Change FileHandlerImpl in order to implement readFromFile()

--- src/main/java/it/unibo/oop/lastcrown/model/file_handling/impl/FileHandlerImpl.java
+++ src/main/java/it/unibo/oop/lastcrown/model/file_handling/impl/FileHandlerImpl.java
@@ -1,6 +1,16 @@
 package it.unibo.oop.lastcrown.model.file_handling.impl;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import it.unibo.oop.lastcrown.model.file_handling.api.FileHandler;
 import it.unibo.oop.lastcrown.model.file_handling.api.Parser;
@@ -12,7 +22,9 @@ import it.unibo.oop.lastcrown.model.file_handling.api.Serializer;
  * @param <T> the type of object to read and write.
  */
 public class FileHandlerImpl<T> implements FileHandler<T> {
-    
+
+    private static final Logger LOGGER = Logger.getLogger(FileHandlerImpl.class.getName());
+
     private final Parser<T> parser;
     private final Serializer<T> serializer;
     private final String baseDirectory;
@@ -30,10 +42,38 @@ public class FileHandlerImpl<T> implements FileHandler<T> {
         this.baseDirectory = baseDirectory;
     }
 
+    /**
+     * Reads an object of type T from the file named "name.txt" in the base directory.
+     *
+     * @param name the key used to build the file name.
+     * @return an Optional of the object read from the file, or an empty Optional if the file doesn't exist or an error occurs.
+     */
     @Override
-    public Optional<T> readFromFile(final String fileName) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'readFromFile'");
+    public Optional<T> readFromFile(final String name) {
+        final String fileName = name + ".txt";
+        final File file = new File(baseDirectory, fileName);
+        if (!file.exists()) {
+            return Optional.empty();
+        }
+        final List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                    new FileInputStream(file), StandardCharsets.UTF_8))) {
+            String line = reader.readLine();
+            while (line != null) {
+                lines.add(line);
+                line = reader.readLine();
+            }
+        } catch (final IOException e) {
+            LOGGER.log(Level.SEVERE, "Error reading from file: " + file.getAbsolutePath(), e);
+            return Optional.empty();
+        }
+        try {
+            return Optional.of(parser.parse(lines));
+        } catch (final IllegalArgumentException e) {
+            LOGGER.log(Level.WARNING, "Error parsing file: " + file.getAbsolutePath(), e);
+            return Optional.empty();
+        }
     }
 
     @Override
