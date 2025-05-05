3aed7f1a6b1ab3c9297fc341d8b41aea39273dd8
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-05-05 21:32:47 +0200

    Add interface CompleteCollection

---
+++ src/main/java/it/unibo/oop/lastcrown/model/user/api/CompleteCollection.java
@@ -0,0 +1,53 @@
+package it.unibo.oop.lastcrown.model.user.api;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import it.unibo.oop.lastcrown.model.card.CardIdentifier;
+import it.unibo.oop.lastcrown.model.characters.api.Hero;
+import it.unibo.oop.lastcrown.model.characters.impl.playablecharacter.PlayableCharacterImpl;
+import it.unibo.oop.lastcrown.model.spell.api.Spell;
+
+/**
+ * Provides method to work  with the entire collection of cards available in the game.
+ */
+public interface CompleteCollection {
+    /**
+     * Provides all the {@link CardIdentifier} in the collection.
+     * 
+     * @return a list with all the {@link CardIdentifier}
+     */
+    List<CardIdentifier> getCompleteCollection();
+
+    /**
+     * Provides the {@link CardIdentifier} of all the cards that have a cost equals to zero.
+     * 
+     * @return a set with all the requested {@link CardIdentifier}
+     */
+    Set<CardIdentifier> getZeroCostCards();
+
+    /**
+     * Provides a {@link Hero} relative to a given {@link CardIdentifier}.
+     * 
+     * @param cardIdentifier the {@link CardIdentifier} to use
+     * @return an {@link Optional} of the requested {@link Hero} if it is found, an empty Optional otherwise
+     */
+    Optional<Hero> getHero(CardIdentifier cardIdentifier);
+
+    /**
+     * Provides a {@link PlayableCharacter} relative to a given {@link CardIdentifier}.
+     * 
+     * @param cardIdentifier the {@link CardIdentifier} to use
+     * @return an {@link Optional} of the requested {@link PlayableCharacter} if it is found, an empty Optional otherwise
+     */
+    Optional<PlayableCharacterImpl> getPlayableCharacter(CardIdentifier cardIdentifier);
+
+    /**
+     * Provides a {@link Spell} relative to a given {@link CardIdentifier}.
+     * 
+     * @param cardIdentifier the {@link CardIdentifier} to use
+     * @return an {@link Optional} of the requested {@link Spell} if it is found, an empty Optional otherwise
+     */
+    Optional<Spell> getSpell(CardIdentifier cardIdentifier);
+}
