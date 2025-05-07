f440c508af6dcaf00b6144fa565bbe58e9a5862f
Giulia <giulia.albertini3@studio.unibo.it>
2025-05-07 17:57:22 +0200

    Add event type

--- src/main/java/it/unibo/oop/lastcrown/model/api/EventType.java
+++ src/main/java/it/unibo/oop/lastcrown/model/api/EventType.java
@@ -4,11 +4,14 @@ package it.unibo.oop.lastcrown.model.api;
  * Represents the different types of events that can occur in the game related to collisions or input.
  */
 public enum EventType {
-
     /**
-     * Event triggered when two entities collide.
+     * Event triggered when player engages an enemy.
      */
-    ENTITY_COLLISION,
+    FOLLOW_ENEMY,
+    /**
+     * Event triggered when player collides with enemy.
+     */
+    ENEMY_COLLISION,
 
     /**
      * Event triggered when an hero collides with a shop.
