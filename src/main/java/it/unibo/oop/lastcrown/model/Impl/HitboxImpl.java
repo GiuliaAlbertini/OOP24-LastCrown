9eba1a0dcba17297170a21dc6017b47dab97c97e
Giulia <giulia.albertini3@studio.unibo.it>
2025-05-07 17:51:48 +0200

    Add methods for radius implementation

--- src/main/java/it/unibo/oop/lastcrown/model/Impl/HitboxImpl.java
+++ src/main/java/it/unibo/oop/lastcrown/model/Impl/HitboxImpl.java
@@ -1,4 +1,4 @@
-package it.unibo.oop.lastcrown.model.Impl;
+package it.unibo.oop.lastcrown.model.impl;
 
 import it.unibo.oop.lastcrown.model.api.Hitbox;
 import it.unibo.oop.lastcrown.model.api.Point2D;
@@ -20,19 +20,40 @@ public class HitboxImpl implements Hitbox{
        return this.position;
     }
 
+    @Override
+    public void setPosition(Point2D newPos) {
+        this.position = newPos;
+    }
+
+    @Override
+    public int getWidth(){
+        return this.width;
+    }
+
+    @Override
+    public int getHeight(){
+        return this.height;
+    }
+
+    @Override
+    public Point2D getCenter() {
+        double centerX = this.position.x() + this.width / 2.0;
+        double centerY = this.position.y() + this.height / 2.0;
+        return new Point2DImpl(centerX, centerY);
+    }
 
     @Override
     //implementazione Axis-Aligned Bounding Box 
-    public boolean checkCollision(Hitbox a, Hitbox b){
-        double ax1= a.getPosition().x();
-        double ay1= a.getPosition().y();
-        double ax2= ax1+width;
-        double ay2= ay1+height;
+    public boolean checkCollision(Hitbox other){
+        double ax1= this.getPosition().x();
+        double ay1= this.getPosition().y();
+        double ax2= ax1+this.width;
+        double ay2= ay1+this.height;
 
-        double bx1= b.getPosition().x();
-        double by1= b.getPosition().y();
-        double bx2= bx1+width;
-        double by2= by1+height;
+        double bx1= other.getPosition().x();
+        double by1= other.getPosition().y();
+        double bx2= bx1+other.getWidth();
+        double by2= by1+other.getHeight();
 
         //se tutte le condizioni di non sovrapposizioni sono vere allora non c'è collisione
         return !(ax2 <= bx1 || bx2 <= ax1 || ay2 <= by1 || by2 <= ay1);
