a050d6774e75f8ce4306b669ae8b797e8bbdd6bd
Giulia <giulia.albertini3@studio.unibo.it>
2025-05-09 16:45:49 +0200

    Add MainView interface

---
+++ src/main/java/it/unibo/oop/lastcrown/view/api/MainView.java
@@ -0,0 +1,31 @@
+package it.unibo.oop.lastcrown.view.api;
+import javax.swing.JPanel;
+import it.unibo.oop.lastcrown.controller.impl.GameState;
+
+/**
+ * Interface for the main view of the application.
+ * Manages the different panels associated with various game states.
+ */
+public interface MainView {
+
+    /**
+     * Initializes all the panels required for the different game states.
+     * This method should be called once during the application startup.
+     */
+    void initPanel();
+
+    /**
+     * Displays the panel corresponding to the specified game state.
+     *
+     * @param panel the game state indicating which panel to show
+     */
+    void showPanel(GameState panel);
+
+    /**
+     * Returns the panel associated with the specified game state.
+     *
+     * @param panel the game state whose panel is to be retrieved
+     * @return the JPanel corresponding to the given game state
+     */
+    JPanel getPanel(GameState panel);
+}
