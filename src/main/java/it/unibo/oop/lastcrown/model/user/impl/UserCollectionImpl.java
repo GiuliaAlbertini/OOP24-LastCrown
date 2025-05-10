61aaecbe92ffed83de37a3fed81c1f68bde20a20
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-05-10 18:12:14 +0200

    Change UserCollectionImpl in order to add addListener()

--- src/main/java/it/unibo/oop/lastcrown/model/user/impl/UserCollectionImpl.java
+++ src/main/java/it/unibo/oop/lastcrown/model/user/impl/UserCollectionImpl.java
@@ -2,11 +2,14 @@ package it.unibo.oop.lastcrown.model.user.impl;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import it.unibo.oop.lastcrown.model.card.CardIdentifier;
 import it.unibo.oop.lastcrown.model.user.api.CompleteCollection;
 import it.unibo.oop.lastcrown.model.user.api.UserCollection;
+import it.unibo.oop.lastcrown.model.user.api.UserCollectionListener;
 
 /**
  * Implementation of a {@link UserCollection}.
@@ -17,6 +20,7 @@ public class UserCollectionImpl implements UserCollection {
     private static final Set<CardIdentifier> INITIAL_SET = computeInitialset();
 
     private final Set<CardIdentifier> userCollection;
+    private final List<UserCollectionListener> listeners = new CopyOnWriteArrayList<>();
 
     /**
      * Constructor for a new {@code UserCollectionImpl}.
@@ -35,6 +39,11 @@ public class UserCollectionImpl implements UserCollection {
     @Override
     public final void addCard(final CardIdentifier newCard) {
         this.userCollection.add(newCard);
+    }
+
+    @Override
+    public final void addListener(final UserCollectionListener listener) {
+        this.listeners.add(listener);
     }
 
     private static Set<CardIdentifier> computeInitialset() {
