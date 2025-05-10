c57348286c87ab0dbc65dfabb1e2dc68051c1082
Giulia <giulia.albertini3@studio.unibo.it>
2025-05-10 15:58:51 +0200

    Add CollisionDetectorImpl class

---
+++ src/main/java/it/unibo/oop/lastcrown/model/Impl/CollisionDetectorImpl.java
@@ -0,0 +1,29 @@
+package it.unibo.oop.lastcrown.model.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import it.unibo.oop.lastcrown.model.api.Collidable;
+import it.unibo.oop.lastcrown.model.api.CollisionEvent;
+import it.unibo.oop.lastcrown.model.api.CollisionManager;
+
+public class CollisionDetector {
+    List<Collidable> object = new ArrayList<>();
+    CollisionManager collisionManager;
+
+    public CollisionDetector(CollisionManager collisionManager) {
+        this.collisionManager= collisionManager;
+    }
+
+    public void loaderEntityCollision(){
+        for (int i=0; i< object.size(); i++){
+            for (int j=i+1; j< object.size()-1; j++){
+                Collidable a= object.get(i);
+                Collidable b= object.get(j);
+                if (a.getData().getHitbox().checkCollision(b.getData().getHitbox())){
+                    CollisionEvent collisionevent = new CollisionEventImpl(a, b);
+                    collisionManager.notify(collisionevent);
+                }
+            }
+        }
+    }
+}
