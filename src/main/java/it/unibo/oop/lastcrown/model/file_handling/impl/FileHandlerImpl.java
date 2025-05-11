08bae0dfb6b5ab2225790969b7ab64ca16cb8d78
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-05-11 10:48:39 +0200

    Change FileHandlerImpl to correctly implement FileHandler

--- src/main/java/it/unibo/oop/lastcrown/model/file_handling/impl/FileHandlerImpl.java
+++ src/main/java/it/unibo/oop/lastcrown/model/file_handling/impl/FileHandlerImpl.java
@@ -1,15 +1,11 @@
 package it.unibo.oop.lastcrown.model.file_handling.impl;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
@@ -20,73 +16,54 @@ import it.unibo.oop.lastcrown.model.file_handling.api.Parser;
 import it.unibo.oop.lastcrown.model.file_handling.api.Serializer;
 
 /**
- * A generic file handler for reading and writing objects of type T using a Parser and Serializer.
- *
- * @param <T> the type of object to read and write.
+ * Full read/write handler: delegate reads to {@link BaseFileHandler}
+ * and implement {@code writeToFile()} via the Serializer.
+ * 
+ * @param <T> the type of object this handler reads and writes
  */
-public class FileHandlerImpl<T> implements FileHandler<T> {
+public class FileHandlerImpl<T>
+        extends BaseFileHandler<T>
+        implements FileHandler<T> {
 
     private static final Logger LOGGER = Logger.getLogger(FileHandlerImpl.class.getName());
-
-    private final Parser<T> parser;
     private final Serializer<T> serializer;
-    private final String baseDirectory;
 
     /**
-     * Constructs a GenericFileHandlerImpl with the given parser, serializer, and base directory.
-     *
-     * @param parser the parser to convert file lines into an object.
-     * @param serializer the serializer to convert an object into file lines.
-     * @param baseDirectory the base directory where files are stored.
+     * Constructs a new {@link FileHandler}.
+     * 
+     * @param parser the parser to use
+     * @param serializer the serializer to use
+     * @param baseDirectory the directory to use
      */
-    public FileHandlerImpl(final Parser<T> parser, final Serializer<T> serializer, final String baseDirectory) {
-        this.parser = parser;
+    public FileHandlerImpl(final Parser<T> parser,
+                           final Serializer<T> serializer,
+                           final String baseDirectory) {
+        super(parser, baseDirectory);
         this.serializer = serializer;
-        this.baseDirectory = baseDirectory;
     }
 
     @Override
-    public final Optional<T> readFromFile(final String name) {
-        final String fileName = name + ".txt";
-        final File file = new File(baseDirectory, fileName);
-        if (!file.exists()) {
-            return Optional.empty();
-        }
-        final List<String> lines = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(
-                    new FileInputStream(file), StandardCharsets.UTF_8))) {
-            String line = reader.readLine();
-            while (line != null) {
-                lines.add(line);
-                line = reader.readLine();
-            }
-        } catch (final IOException e) {
-            LOGGER.log(Level.SEVERE, "Error reading from file: " + file.getAbsolutePath(), e);
-            return Optional.empty();
-        }
-        try {
-            return Optional.of(parser.parse(lines));
-        } catch (final IllegalArgumentException e) {
-            LOGGER.log(Level.WARNING, "Error parsing file: " + file.getAbsolutePath(), e);
-            return Optional.empty();
-        }
+    public final Optional<T> readFromFile(final String fileName) {
+        return read(fileName);
     }
 
     @Override
-    public final void writeToFile(final String name, final T object) {
-        final String fileName = name + ".txt";
-        final File file = new File(baseDirectory, fileName);
+    public final void writeToFile(final String fileName, final T object) {
+        final File file = new File(getBaseDirectory(), fileName + ".txt");
         final List<String> content = serializer.serialize(object);
-        try (BufferedWriter writer = new BufferedWriter(
-                new OutputStreamWriter(
-                    new FileOutputStream(file), StandardCharsets.UTF_8))) {
-            for (final String line : content) {
+
+        try (var writer = new BufferedWriter(
+                 new OutputStreamWriter(
+                     new FileOutputStream(file),
+                     StandardCharsets.UTF_8))) {
+            for (final var line : content) {
                 writer.write(line);
                 writer.newLine();
             }
-        } catch (final IOException ex) {
-            LOGGER.log(Level.SEVERE, "Error writing to file: " + file.getAbsolutePath(), ex);
+        } catch (final IOException e) {
+            LOGGER.log(Level.SEVERE,
+                       "Error writing to file: " + file.getAbsolutePath(),
+                       e);
         }
     }
 }
