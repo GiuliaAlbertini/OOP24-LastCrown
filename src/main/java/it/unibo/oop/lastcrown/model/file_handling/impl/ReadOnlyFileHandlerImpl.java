734383a24e5611f3eb1c5599ad0c453a19feee54
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-05-11 10:49:24 +0200

    Change ReadOnlyFileHandlerImpl to extend BaseFileHandler

--- src/main/java/it/unibo/oop/lastcrown/model/file_handling/impl/ReadOnlyFileHandlerImpl.java
+++ src/main/java/it/unibo/oop/lastcrown/model/file_handling/impl/ReadOnlyFileHandlerImpl.java
@@ -1,67 +1,32 @@
 package it.unibo.oop.lastcrown.model.file_handling.impl;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import it.unibo.oop.lastcrown.model.file_handling.api.Parser;
 import it.unibo.oop.lastcrown.model.file_handling.api.ReadOnlyFileHandler;
 
 /**
- * A read-only file handler for reading objects of type T using a Parser.
- *
- * @param <T> the type of object to read.
+ * Read‐only handler: reuses {@link BaseFileHandler} for all reads.
+ * 
+ * @param <T> the type of object this handler reads and writes
  */
-public class ReadOnlyFileHandlerImpl<T> implements ReadOnlyFileHandler<T> {
-
-    private static final Logger LOGGER = Logger.getLogger(ReadOnlyFileHandlerImpl.class.getName());
-
-    private final Parser<T> parser;
-    private final String baseDirectory;
+public class ReadOnlyFileHandlerImpl<T>
+        extends BaseFileHandler<T>
+        implements ReadOnlyFileHandler<T> {
 
     /**
-     * Constructs a {@link ReadOnlyFileHandlerImpl} with the given parser and base directory.
-     *
-     * @param parser the parser to convert file lines into an object.
-     * @param baseDirectory the base directory where files are stored.
+     * Constructs a new {@code ReadOnlyFileHandler}.
+     * 
+     * @param parser the parser to use
+     * @param baseDirectory the directory to use
      */
-    public ReadOnlyFileHandlerImpl(final Parser<T> parser, final String baseDirectory) {
-        this.parser = parser;
-        this.baseDirectory = baseDirectory;
+    public ReadOnlyFileHandlerImpl(final Parser<T> parser,
+                                   final String baseDirectory) {
+        super(parser, baseDirectory);
     }
 
     @Override
-    public final Optional<T> readFromFile(final String name) {
-        final String fileName = name + ".txt";
-        final File file = new File(baseDirectory, fileName);
-        if (!file.exists()) {
-            return Optional.empty();
-        }
-        final List<String> lines = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8))) {
-                    String line = reader.readLine();
-                    while (line != null) {
-                        lines.add(line);
-                        line = reader.readLine();
-                    }
-        } catch (final IOException e) {
-            LOGGER.log(Level.SEVERE, "Error reading from file: " + file.getAbsolutePath(), e);
-            return Optional.empty();
-        }
-        try {
-            return Optional.of(parser.parse(lines));
-        } catch (final IllegalArgumentException e) {
-            LOGGER.log(Level.WARNING, "Error parsing file: " + file.getAbsolutePath(), e);
-            return Optional.empty();
-        }
+    public final Optional<T> readFromFile(final String fileName) {
+        return read(fileName);
     }
 }
