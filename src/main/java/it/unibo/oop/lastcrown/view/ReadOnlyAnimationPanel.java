3daa001a46682d42aa630a4f1e20706dfe3d4c04
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-05-15 23:05:33 +0200

    Add AnimationPanelProxy from branch feature_character and correct gradle warnings

--- src/main/java/it/unibo/oop/lastcrown/view/ReadOnlyAnimationPanel.java
+++ src/main/java/it/unibo/oop/lastcrown/view/ReadOnlyAnimationPanel.java
@@ -6,7 +6,7 @@ import javax.swing.JComponent;
  * An interface that permits to get a read only version of an animation panel.
  */
 public interface ReadOnlyAnimationPanel {
-    
+
     /**
      * @return a read only version of the animation panel.
      */
