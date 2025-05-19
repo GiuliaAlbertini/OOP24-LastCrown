a57ff6bcef7dda26529ad6b12f22c48c10745c47
Giulia <giulia.albertini3@studio.unibo.it>
2025-05-19 14:34:46 +0200

    Add first version HitboxPanel implementation

---
+++ src/main/java/it/unibo/oop/lastcrown/view/impl/HitboxPanelImpl.java
@@ -0,0 +1,38 @@
+package it.unibo.oop.lastcrown.view.impl;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+import it.unibo.oop.lastcrown.model.api.Hitbox;
+import it.unibo.oop.lastcrown.model.impl.Point2DImpl;
+
+public class HitboxPanel {
+    Hitbox hitbox;
+    JPanel hitboxPanel;
+
+    public HitboxPanel(Hitbox hitbox){
+        this.hitbox=hitbox;
+        this.hitboxPanel= new JPanel();
+        this.hitboxPanel.setBackground(new Color(255, 0, 0, 100)); // Rosso semi-trasparente per debug
+        updatePanel();
+    }
+
+    public JPanel getHitboxPanel(){
+        return this.hitboxPanel;
+    }
+
+
+    public void updatePanel(){
+        hitboxPanel.setBounds(
+            (int) hitbox.getPosition().x(),
+            (int) hitbox.getPosition().y(),                
+            hitbox.getWidth(),
+            hitbox.getHeight()
+        );
+    }
+    
+    public void setPanelPosition(int x, int y){
+        hitbox.setPosition(new Point2DImpl(x, y));
+        updatePanel();
+    }
+}
