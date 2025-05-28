5d68254a7c32b90f0eaa097d7f3da86269817ba9
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-05-28 19:55:30 +0200

    Change Spell interface and Spell implementation by removing actionRange and duration

--- src/main/java/it/unibo/oop/lastcrown/model/spell/api/SpellEffect.java
+++ src/main/java/it/unibo/oop/lastcrown/model/spell/api/SpellEffect.java
@@ -1,7 +1,5 @@
 package it.unibo.oop.lastcrown.model.spell.api;
 
-import java.util.Optional;
-
 import it.unibo.oop.lastcrown.model.card.CardType;
 
 /**
@@ -9,8 +7,6 @@ import it.unibo.oop.lastcrown.model.card.CardType;
  * @param category the category of character statistic to which the effect is aimed (health, attack...).
  * @param amount the amount of change to a specific character statistic caused by the effect (+200 health, +3 attack).
  * @param target the target of the effect (friendly characters or enemies).
- * @param duration the duration of the effect expressed in seconds. If it is missing,
- * the effect is activated as soon as the spell is cast.
  */
-public record SpellEffect(String category, int amount, CardType target, Optional<Integer> duration) {
+public record SpellEffect(String category, int amount, CardType target) {
 }
