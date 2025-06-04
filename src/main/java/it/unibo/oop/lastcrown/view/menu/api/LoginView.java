b73ac80b5cf3e3f6d6da0b63c18d5358679af593
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-06-04 11:02:01 +0200

    Add interface LoginView

---
+++ src/main/java/it/unibo/oop/lastcrown/view/menu/api/LoginView.java
@@ -0,0 +1,13 @@
+package it.unibo.oop.lastcrown.view.menu.api;
+
+/**
+ * Rapresentation of a {@code LoginView}.
+ */
+public interface LoginView {
+    /**
+     * Changes the visibility of {@code LoginView}.
+     * 
+     * @param flag {@code True} if the visibility has to be set, {@code False} otherwise
+     */
+    void setVisibility(boolean flag);
+}
