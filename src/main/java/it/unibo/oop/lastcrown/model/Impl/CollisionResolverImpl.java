ddc8f2332d3b30d1d9c953672eabcde9db0660ab
Giulia <giulia.albertini3@studio.unibo.it>
2025-06-06 15:23:01 +0200

    Add CollisionResolver implementation

---
+++ src/main/java/it/unibo/oop/lastcrown/model/Impl/CollisionResolverImpl.java
@@ -0,0 +1,62 @@
+package it.unibo.oop.lastcrown.model.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import it.unibo.oop.lastcrown.model.api.Collidable;
+import it.unibo.oop.lastcrown.model.api.CollisionEvent;
+import it.unibo.oop.lastcrown.model.api.CollisionResolver;
+import it.unibo.oop.lastcrown.model.api.Point2D;
+import it.unibo.oop.lastcrown.model.impl.Handler.HandleFollowEnemy;
+import it.unibo.oop.lastcrown.view.characters.api.CharacterMovementObserver;
+
+public class CollisionResolverImpl implements CollisionResolver {
+	private final Map<Integer, HandleFollowEnemy> activeFollowMovements = new HashMap<>();
+	CharacterMovementObserver movbs;
+	CharacterMovementStop movbstop;
+	
+	public CollisionResolverImpl(CharacterMovementObserver movbs, CharacterMovementStop movbstop){
+		this.movbs=movbs;
+		this.movbstop=movbstop;
+	}
+
+	@Override
+	public void notify(CollisionEvent event) {
+		switch (event.getType()) {
+			case FOLLOW_ENEMY -> handleFollowEnemy(event);
+			default -> System.out.println("[WARN] Evento collisione non gestito: " + event.getType());
+		}
+	}
+
+	private void handleFollowEnemy(CollisionEvent event) {
+		//System.out.println("sono nel resolver");
+		int characterId = event.getCollidable1().getCardidentifier().number();
+		HandleFollowEnemy movement = new HandleFollowEnemy(event, movbs, movbstop);
+		movement.updateMovement(0);
+		activeFollowMovements.put(characterId, movement);
+	}
+
+	public List<Collidable> updateAllMovements(long deltaMs) {
+		List<Collidable> updatedCharacters = new ArrayList<>();
+
+		for (final var handle : activeFollowMovements.entrySet()) {
+			HandleFollowEnemy movement = handle.getValue();
+			movement.updateMovement(deltaMs);
+			updatedCharacters.add(movement.getCharacter());
+		}
+		return updatedCharacters;
+	}
+
+	@Override
+	public Point2D getCharacterPosition(int characterId) {
+		HandleFollowEnemy movement = activeFollowMovements.get(characterId);
+		//System.out.println("sono in getCharacterPosition");
+		if (movement != null) {
+			return movement.getCurrentPosition();
+		} else {
+			return null; 
+		}	
+	}	
+}
