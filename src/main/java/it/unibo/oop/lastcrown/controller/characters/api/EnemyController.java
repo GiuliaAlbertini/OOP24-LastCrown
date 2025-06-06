2a91aee3446599dbb77b84af1c60a47b7c3d690f
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-06-06 13:50:22 +0200

    Refactory of EnemyController interface, implementation and static factory

--- src/main/java/it/unibo/oop/lastcrown/controller/characters/api/EnemyController.java
+++ src/main/java/it/unibo/oop/lastcrown/controller/characters/api/EnemyController.java
@@ -4,9 +4,4 @@ package it.unibo.oop.lastcrown.controller.characters.api;
  * A controller that handles the behaviour of a single enemy in game.
  */
 public interface EnemyController extends GenericCharacterController {
-
-    /**
-     * Make this enemy retreat.
-     */
-    void retreat();
 }
