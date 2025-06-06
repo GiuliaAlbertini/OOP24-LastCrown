ed9817f6437d54138d49024aa2ca28b73d932e80
Giulia <giulia.albertini3@studio.unibo.it>
2025-06-06 15:10:59 +0200

    Change Collidable interface

--- src/main/java/it/unibo/oop/lastcrown/model/api/Collidable.java
+++ src/main/java/it/unibo/oop/lastcrown/model/api/Collidable.java
@@ -1,6 +1,6 @@
 package it.unibo.oop.lastcrown.model.api;
 
-import it.unibo.oop.lastcrown.model.characters.api.InGameCharacter;
+import it.unibo.oop.lastcrown.model.card.CardIdentifier;
 
 /**
  * Represents an entity that can be involved in collisions.
@@ -8,15 +8,7 @@ import it.unibo.oop.lastcrown.model.characters.api.InGameCharacter;
  * which includes its position, hitbox, and other relevant properties.
  */
 public interface Collidable {
-
-    /**
-     * Returns the underlying data of the in-game character associated
-     * with this collidable object. This data includes the character's
-     * hitbox, position, and potentially other attributes useful for
-     * collision resolution and game logic.
-     *
-     * @return InGameCharacter associated with this collidable entity
-     */
-    InGameCharacter getData();
+    public Hitbox getHitbox();
+    public CardIdentifier getCardidentifier();
 }
 
