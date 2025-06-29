c7df0214fcc5949e3e750f46af52374381f9ed02
Giulia <giulia.albertini3@studio.unibo.it>
2025-06-29 17:19:19 +0200

    Change DeadHandler implementation

--- src/main/java/it/unibo/oop/lastcrown/controller/Impl/handlercontroller/DeadHandler.java
+++ src/main/java/it/unibo/oop/lastcrown/controller/Impl/handlercontroller/DeadHandler.java
@@ -1,26 +1,46 @@
 package it.unibo.oop.lastcrown.controller.impl.handlercontroller;
 
+import it.unibo.oop.lastcrown.controller.api.MatchController;
 import it.unibo.oop.lastcrown.controller.characters.api.GenericCharacterController;
 import it.unibo.oop.lastcrown.controller.impl.eventcharacters.CharacterState;
+import it.unibo.oop.lastcrown.controller.impl.eventcharacters.EventFactory;
 import it.unibo.oop.lastcrown.controller.impl.eventcharacters.EventQueue;
 import it.unibo.oop.lastcrown.controller.impl.eventcharacters.StateHandler;
 import it.unibo.oop.lastcrown.view.characters.Keyword;
 
 /**
  * StateHandler implementation that manages the "dead" state of a character.
- * 
+ *
  * This handler sets the death animation for the character and iterates through all
  * animation frames before confirming the character is dead.
  */
 public final class DeadHandler implements StateHandler {
+    private final MatchController match;
+    private final EventFactory eventFactory;
+    int i;
+
+    public DeadHandler(final MatchController matchController, final EventFactory eventFactory) {
+        this.match = matchController;
+        this.eventFactory=eventFactory;
+    }
 
     @Override
     public CharacterState handle(final GenericCharacterController character, final EventQueue queue, final int deltaTime) {
         final int frame = character.getDeathAnimationSize();
         character.setNextAnimation(Keyword.DEATH);
-        for (int i = 0; i < frame; i++) {
+        while (i < frame) {
             character.showNextFrame();
+            i++;
+
+            if (i==frame){
+                match.removeCharacterCompletelyById(character.getId().number());
+            }
+            queue.enqueue(eventFactory.createEvent(CharacterState.DEAD));
+            return CharacterState.DEAD;
         }
+
+        queue.enqueue(eventFactory.createEvent(CharacterState.DEAD));
         return CharacterState.DEAD;
     }
+
 }
