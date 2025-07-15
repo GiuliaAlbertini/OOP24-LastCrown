936955b1516490f8cdc43719932dbe9180f8e250
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-07-15 16:45:27 +0200

    Change TraderPanel by adding MouseListener

--- src/main/java/it/unibo/oop/lastcrown/view/shop/TraderPanel.java
+++ src/main/java/it/unibo/oop/lastcrown/view/shop/TraderPanel.java
@@ -2,12 +2,15 @@ package it.unibo.oop.lastcrown.view.shop;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.util.List;
 import java.util.logging.Logger;
 
 import javax.swing.JPanel;
 
+import it.unibo.oop.lastcrown.model.card.CardType;
 import it.unibo.oop.lastcrown.view.CustomLock;
 import it.unibo.oop.lastcrown.view.ImageLoader;
 import it.unibo.oop.lastcrown.view.characters.CharacterPathLoader;
@@ -21,6 +24,9 @@ public final class TraderPanel extends JPanel {
     private static final Logger LOG = Logger.getLogger(TraderPanel.class.getName());
     private static final String TRADER = "trader";
     private static final int TIME = 100;
+    private int id;
+    private ContainerObserver obs;
+    private CardType type;
     private volatile boolean done;
     private int cont;
     private transient CustomLock lock;
@@ -32,31 +38,45 @@ public final class TraderPanel extends JPanel {
     private TraderPanel() { }
 
     /**
+     * @param id the numerical id of this trader
+     * @param obs the observer of the container
      * @param name the name of the trader
+     * @param type the CardType that this trader will show
      * @param width the width of the trader panel
      * @param height the height of the trader panel
      * @return a new TraderPanel
      */
-    public static TraderPanel create(final String name, final int width, final int height) {
+    public static TraderPanel create(final int id, final ContainerObserver obs, final String name,
+     final CardType type, final int width, final int height) {
         final TraderPanel instance = new TraderPanel();
-        instance.init(name, width, height);
+        instance.init(id, obs, name, type, width, height);
         return instance;
     }
 
-    private void init(final String name, final int width, final int height) {
-        this.stopImages = ImageLoader.getAnimationFrames(CharacterPathLoader.loadPaths(TRADER, name, Keyword.STOP.get()),
-         width, height);
-        this.openImages = ImageLoader.getAnimationFrames(CharacterPathLoader.loadPaths(TRADER, name, Keyword.OPEN.get()),
-         width, height);
-        this.closeImages = ImageLoader.getAnimationFrames(CharacterPathLoader.loadPaths(TRADER, name, Keyword.CLOSE.get()),
-         width, height);
-        this.approvalImages = ImageLoader.getAnimationFrames(CharacterPathLoader.loadPaths(TRADER, name, Keyword.APPROVAL.get()),
-         width, height);
+    private void init(final int id, final ContainerObserver obs, final String name, final CardType type,
+     final int width, final int height) {
+        this.id = id;
+        this.obs = obs;
+        this.type = type;
+        this.stopImages = ImageLoader.getAnimationFrames(CharacterPathLoader.loadPaths(
+            TRADER, name, Keyword.STOP.get()), width, height);
+        this.openImages = ImageLoader.getAnimationFrames(CharacterPathLoader.loadPaths(
+            TRADER, name, Keyword.OPEN.get()), width, height);
+        this.closeImages = ImageLoader.getAnimationFrames(CharacterPathLoader.loadPaths(
+            TRADER, name, Keyword.CLOSE.get()), width, height);
+        this.approvalImages = ImageLoader.getAnimationFrames(CharacterPathLoader.loadPaths(
+            TRADER, name, Keyword.APPROVAL.get()), width, height);
         this.lock = new CustomLock();
         this.setLayout(null);
         this.setFocusable(false);
         this.setOpaque(false);
         this.setSize(width, height);
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(final MouseEvent e) {
+                startAnimation(openImages, Keyword.OPEN);
+            }
+        });
     }
 
     /**
@@ -103,7 +123,7 @@ public final class TraderPanel extends JPanel {
         this.startAnimation(this.approvalImages, Keyword.APPROVAL);
     }
 
-    private void startAnimation(final List<BufferedImage> frames, final Keyword type) {
+    private void startAnimation(final List<BufferedImage> frames, final Keyword keyword) {
         this.done = true;
         new Thread(() -> {
             this.lock.acquireLock();
@@ -117,16 +137,18 @@ public final class TraderPanel extends JPanel {
                 }
             }
             this.lock.releaseLock();
-            if (type.equals(Keyword.CLOSE)) {
+            if (keyword.equals(Keyword.OPEN)) {
+                this.obs.notifyInteraction(this.id, this.type);
+            } else if (keyword.equals(Keyword.CLOSE)) {
                 this.startStopLoop();
-            } else if (type.equals(Keyword.APPROVAL)) {
+            } else if (keyword.equals(Keyword.APPROVAL)) {
                 this.startCloseSequence();
             }
         }).start();
     }
 
     /**
-     * Must be called when the shop JFrame is not visible. Stops the animation of this 
+     * Must be called when the shop JFrame is not visible. Stops the animation of this trader.
      */
     public void stopAnimations() {
         this.done = true;
