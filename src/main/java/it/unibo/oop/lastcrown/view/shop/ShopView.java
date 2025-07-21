381b2c20be8eb238a2d80e08c2e2a42592b0e6b9
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-07-21 10:45:01 +0200

    Change ShopView by removing addHeroPanel() method

--- src/main/java/it/unibo/oop/lastcrown/view/shop/ShopView.java
+++ src/main/java/it/unibo/oop/lastcrown/view/shop/ShopView.java
@@ -1,20 +1,11 @@
 package it.unibo.oop.lastcrown.view.shop;
 
-import javax.swing.JComponent;
-
 import it.unibo.oop.lastcrown.view.menu.api.Scene;
 
 /**
  * An interface that models the shop view.
  */
 public interface ShopView extends Scene {
-    /**
-     * Adds the hero panel to the shop by specifing if it is the beginning of the game.
-     * @param heroComp the JComponent where the hero animation is located
-     * @param beginning True if it is the beginning of the game, false otherwise
-     */
-    void addHeroPanel(JComponent heroComp, boolean beginning);
-
     /**
      * Notifies to the shop JFrame that it is visible to the player.
      */
