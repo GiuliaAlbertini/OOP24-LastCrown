4839bf7195d02bf52c568ea5a85738744f3d82bb
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-07-21 13:46:32 +0200

    Add MainView and Scene interfaces from other branches

---
+++ src/main/java/it/unibo/oop/lastcrown/view/menu/api/MainView.java
@@ -0,0 +1,32 @@
+package it.unibo.oop.lastcrown.view.menu.api;
+
+import java.util.Set;
+
+import it.unibo.oop.lastcrown.model.card.CardIdentifier;
+
+/**
+ * Defines the main view of the application.
+ */
+public interface MainView {
+
+    /**
+     * Changes the currently displayed panel to the one corresponding to the given scene name.
+     *
+     * @param sceneCaller the name of the calling scene.
+     * @param sceneDestination the name of the scene to be displayed,
+     */
+    void changePanel(String sceneCaller, String sceneDestination);
+
+    /**
+     * Calls the close of the application.
+     */
+    void closeApplication();
+
+    /**
+     * Update the DeckView in use.
+     * 
+     * @param newSet the updated collection to show
+     */
+    void updateDeckView(Set<CardIdentifier> newSet);
+}
+
