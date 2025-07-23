e83bcc4f0abf3aa58aab0c888ebd335b31521865
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-07-23 20:44:01 +0200

    Refactor to remove redundant methods

--- src/main/java/it/unibo/oop/lastcrown/controller/app_managing/impl/InGameAccountManager.java
+++ src/main/java/it/unibo/oop/lastcrown/controller/app_managing/impl/InGameAccountManager.java
@@ -13,8 +13,9 @@ import it.unibo.oop.lastcrown.model.user.impl.CompleteCollectionImpl;
  * Controller to handle account changes during a match.
  */
 public final class InGameAccountManager {
-    private final Account tempAccount;
     private static final CompleteCollection COMPLETE_COLLECTION = new CompleteCollectionImpl();
+
+    private final Account account;
 
     /**
      * Private constructor to initialize the manager with a copy of the given account.
@@ -22,7 +23,7 @@ public final class InGameAccountManager {
      * @param original the account to copy and manage
      */
     private InGameAccountManager(final Account original) {
-        this.tempAccount = createAccountCopy(original);
+        this.account = createAccountCopy(original);
     }
 
     /**
@@ -33,88 +34,6 @@ public final class InGameAccountManager {
      */
     public static InGameAccountManager create(final Account original) {
         return new InGameAccountManager(original);
-    }
-
-    /**
-     * Tells if a card is buyable.
-     * 
-     * @param id the card to check
-     * @return True if the card is buyable, False otherwise
-     */
-    public Boolean isBuyable(final CardIdentifier id) {
-        return substractCoinsAndCost(id) >= 0;
-    }
-
-    private int substractCoinsAndCost(final CardIdentifier id) {
-        return this.tempAccount.getCoins() - getCardCost(id);
-    }
-
-    private Integer getCardCost(final CardIdentifier id) {
-        return COMPLETE_COLLECTION.getCost(id);
-    }
-
-    /**
-     * Adds coins to the managed account.
-     *
-     * @param amount the number of coins to add
-     */
-    public void addCoins(final int amount) {
-        tempAccount.addCoins(amount);
-    }
-
-    /**
-     * Removes coins from the managed account.
-     *
-     * @param amount the number of coins to remove
-     */
-    public void removeCoins(final int amount) {
-        tempAccount.removeCoins(amount);
-    }
-
-    /**
-     * Records the defeat of a boss for the managed account.
-     */
-    public void recordBossDefeat() {
-        tempAccount.increaseBossesDefeated();
-    }
-
-    /**
-     * Records the playing of a game for the managed account.
-     */
-    public void recordGamePlayed() {
-        tempAccount.increasePlayedGames();
-    }
-
-    /**
-     * Adds playtime to the managed account.
-     *
-     * @param hours the amount of playtime to add, in hours
-     */
-    public void addPlaytime(final double hours) {
-        tempAccount.addPlaytime(hours);
-    }
-
-    public Set<CardIdentifier> getUserCollection() {
-        return this.tempAccount.getUserCollection().getCollection();
-    }
-
-    /**
-     * Adds a new card to the managed account's collection.
-     *
-     * @param newCard the identifier of the card to add
-     */
-    public void addCard(final CardIdentifier newCard) {
-        tempAccount.addCard(newCard);
-        this.removeCoins(getCardCost(newCard));
-    }
-
-    /**
-     * Returns a safe copy of the managed account.
-     *
-     * @return a copy of the current state of the managed account
-     */
-    public Account getManagedAccount() {
-        return createAccountCopy(this.tempAccount);
     }
 
     /**
@@ -143,5 +62,50 @@ public final class InGameAccountManager {
             copy.addCard(id);
         }
         return copy;
+    }
+
+    /**
+     * Tells if a card is buyable.
+     * 
+     * @param id the card to check
+     * @return True if the card is buyable, False otherwise
+     */
+    public Boolean isBuyable(final CardIdentifier id) {
+        return substractCoinsAndCost(id) >= 0;
+    }
+
+    /**
+     * Getter for the account collection.
+     * @return the collection
+     */
+    public Set<CardIdentifier> getUserCollection() {
+        return this.account.getUserCollection().getCollection();
+    }
+
+    /**
+     * Adds a new card to the managed account's collection.
+     *
+     * @param newCard the identifier of the card to add
+     */
+    public void addCard(final CardIdentifier newCard) {
+        this.account.addCard(newCard);
+        this.account.removeCoins(getCardCost(newCard));
+    }
+
+    /**
+     * Returns a safe copy of the managed account.
+     *
+     * @return a copy of the current state of the managed account
+     */
+    public Account getManagedAccount() {
+        return createAccountCopy(this.account);
+    }
+
+    private int substractCoinsAndCost(final CardIdentifier id) {
+        return this.account.getCoins() - getCardCost(id);
+    }
+
+    private Integer getCardCost(final CardIdentifier id) {
+        return COMPLETE_COLLECTION.getCost(id);
     }
 }
