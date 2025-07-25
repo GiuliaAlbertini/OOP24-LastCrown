d473852768b44afe1f7cec4ccd01c904ef888d67
Giulia <giulia.albertini3@studio.unibo.it>
2025-07-25 10:59:03 +0200

    Change the MainController interface to return the MatchController instance

--- src/main/java/it/unibo/oop/lastcrown/controller/app_managing/api/MainController.java
+++ src/main/java/it/unibo/oop/lastcrown/controller/app_managing/api/MainController.java
@@ -3,6 +3,7 @@ package it.unibo.oop.lastcrown.controller.app_managing.api;
 import java.util.Optional;
 import java.util.Set;
 
+import it.unibo.oop.lastcrown.controller.collision.api.MatchController;
 import it.unibo.oop.lastcrown.model.card.CardIdentifier;
 import it.unibo.oop.lastcrown.model.user.api.Account;
 
@@ -17,22 +18,31 @@ public interface MainController {
     void closeAll();
 
     /**
-     * Notify the classes that uses the user's collection to update it with the new set.
-     * 
+     * Notify the classes that uses the user's collection to update it with the new
+     * set.
+     *
      * @param newSet the new set to use
      */
     void updateUserColletionUsers(Set<CardIdentifier> newSet);
 
     /**
      * Sets the necessary aspects to go in the Menu section.
-     * 
+     *
      * @param username the username representing the account to use
      */
     void goOverLogin(String username);
 
     /**
      * Getter for the account in use.
+     *
      * @return an optional containing the account
      */
     Optional<Account> getAccount();
+
+    /**
+     * Returns the current {@link MatchController} instance.
+     *
+     * @return the active MatchController
+     */
+    MatchController getMatchController();
 }
