6010a4e8e6bfc77b45df9f60137ae60ad01a8e55
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-07-26 21:28:37 +0200

    Add missing javadoc

--- src/main/java/it/unibo/oop/lastcrown/view/menu/api/DeckViewInterface.java
+++ src/main/java/it/unibo/oop/lastcrown/view/menu/api/DeckViewInterface.java
@@ -1,5 +1,8 @@
 package it.unibo.oop.lastcrown.view.menu.api;
 
+/**
+ * Interface for the view of the deck.
+ */
 public interface DeckViewInterface extends ModifiableBackScene {
     /**
      * SHow the rules to build a deck.
