4d5355fc67df39159f5f99a9d82d23ef27312571
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-07-26 21:43:34 +0200

    Refactor to correct graphics elements

--- src/main/java/it/unibo/oop/lastcrown/view/menu/impl/CollectionView.java
+++ src/main/java/it/unibo/oop/lastcrown/view/menu/impl/CollectionView.java
@@ -3,12 +3,6 @@ package it.unibo.oop.lastcrown.view.menu.impl;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Toolkit;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -24,38 +18,34 @@ import it.unibo.oop.lastcrown.model.card.CardIdentifier;
 import it.unibo.oop.lastcrown.model.card.CardType;
 import it.unibo.oop.lastcrown.view.SceneName;
 import it.unibo.oop.lastcrown.view.menu.api.ModifiableBackScene;
+import it.unibo.oop.lastcrown.view.scenes_utilities.BackButton;
+import it.unibo.oop.lastcrown.view.scenes_utilities.CardsGridPanel;
+import it.unibo.oop.lastcrown.view.scenes_utilities.DetailPanel;
+import it.unibo.oop.lastcrown.view.scenes_utilities.FilterPanel;
+import it.unibo.oop.lastcrown.view.scenes_utilities.HideableScrollPane;
 
 /**
  * View that shows the complete collection.
  */
 public final class CollectionView extends AbstractScene implements ModifiableBackScene {
-    private static final int BTN_FONT_SIZE = 20;
     private static final long serialVersionUID = 1L;
-    private static final int FILTER_BAR_HGAP = 8;
     private static final double DETAIL_RATIO = 0.30;
-    private static final int FILTER_BUTTON_HEIGHT = 36;
     private static final int MIN_CELL_SIDE = 200;
     private static final int GRID_HGAP = 10;
     private static final int GRID_VGAP = 10;
-    private static final CardType[] TYPES = {
-        CardType.HERO, CardType.MELEE, CardType.RANGED, CardType.SPELL,
-    };
-    private static final int BASE_SCREEN_WIDTH = 1920;
-    private static final String FONT_NAME = "SansSerif";
-    private static final int SCREEN_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
 
     private final transient CollectionController collectionController;
     private final transient SceneManager sceneManager;
 
     private final JPanel mainContainer;
-    private final JPanel detailPanel;
+    private final DetailPanel detailPanel;
     private final JPanel rightContainer;
-    private final JPanel filterBar;
-    private final CardGridPanel cardsGridPanel;
+    private final FilterPanel filterPanel;
+    private final CardsGridPanel cardsGridPanel;
     private final BackButton backButton;
 
-    private int detailWidth;
-    private int gridColumns = 1;
+    private final int detailWidth;
+    private int gridColumns = 5;
     private transient Optional<CardType> currentFilter = Optional.empty();
 
     private CollectionView(final SceneManager sceneManager,
@@ -63,23 +53,16 @@ public final class CollectionView extends AbstractScene implements ModifiableBac
                            final Set<CardIdentifier> cardsOwned) {
         this.sceneManager = sceneManager;
         this.collectionController = collectionController;
-        detailPanel = new JPanel(new BorderLayout());
+        detailPanel = new DetailPanel();
 
-        filterBar = new JPanel(new FlowLayout(FlowLayout.CENTER, FILTER_BAR_HGAP, 0));
-        filterBar.setMinimumSize(new Dimension(0, FILTER_BUTTON_HEIGHT));
-        filterBar.setMaximumSize(new Dimension(Integer.MAX_VALUE, FILTER_BUTTON_HEIGHT));
+        filterPanel = new FilterPanel(typeOpt -> loadCards(typeOpt, cardsOwned));
 
-        addFilterButton("ALL", Optional.empty(), cardsOwned);
-        for (final CardType type : TYPES) {
-            addFilterButton(type.get(), Optional.of(type), cardsOwned);
-        }
-
-        cardsGridPanel = CardGridPanel.create(gridColumns, GRID_HGAP, GRID_VGAP);
+        cardsGridPanel = CardsGridPanel.create(gridColumns, GRID_HGAP, GRID_VGAP);
         final var scrollPane = new HideableScrollPane(cardsGridPanel);
 
         rightContainer = new JPanel();
         rightContainer.setLayout(new BoxLayout(rightContainer, BoxLayout.Y_AXIS));
-        rightContainer.add(filterBar);
+        rightContainer.add(filterPanel);
         rightContainer.add(scrollPane);
 
         mainContainer = new JPanel(new BorderLayout());
@@ -95,22 +78,16 @@ public final class CollectionView extends AbstractScene implements ModifiableBac
         add(south, BorderLayout.SOUTH);
         setComponentsOpacity(backButton);
 
-        addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(final ComponentEvent e) {
-                final int totalW = getWidth();
-                detailWidth = (int) Math.round(totalW * DETAIL_RATIO);
-                detailPanel.setPreferredSize(new Dimension(detailWidth, getHeight()));
-                detailPanel.revalidate();
-
-                final int availableW = totalW - detailWidth;
-                final int columns = Math.max(1, (availableW + GRID_HGAP) / (MIN_CELL_SIDE + GRID_HGAP));
-                if (columns != gridColumns) {
-                    gridColumns = columns;
-                }
-                cardsGridPanel.updateGridDimensions(availableW, MIN_CELL_SIDE);
-            }
-        });
+        int initWidth = this.getWidth();
+        if (initWidth <= 0) {
+            initWidth = SCREEN_WIDTH;
+        }
+        detailWidth = (int) Math.round(initWidth * DETAIL_RATIO);
+        detailPanel.setPreferredSize(new Dimension(detailWidth, this.getHeight()));
+        final int availableW = initWidth - detailWidth;
+        gridColumns = Math.max(1,
+                (availableW + GRID_HGAP) / (MIN_CELL_SIDE + GRID_HGAP));
+        //cardsGridPanel.updateGridDimensions(availableW, MIN_CELL_SIDE);
 
         loadCards(this.currentFilter, cardsOwned);
     }
@@ -154,43 +131,12 @@ public final class CollectionView extends AbstractScene implements ModifiableBac
         final List<CardIdentifier> list = type.isPresent()
             ? collectionController.getCollectionByType(type.get())
             : collectionController.getCompleteCollection().getCompleteCollectionAsSet();
-        cardsGridPanel.loadCards(Collections.unmodifiableList(list), this::showDetail, cardsOwned);
-    }
-
-    private void showDetail(final CardIdentifier card) {
-        detailPanel.removeAll();
-        final var big = CardPanel.create(card);
-        big.setPreferredSize(new Dimension(detailWidth, detailWidth));
-
-        final var wrapper = new JPanel(new GridBagLayout());
-        wrapper.setOpaque(false);
-        final var gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        wrapper.add(big, gbc);
-
-        detailPanel.add(wrapper, BorderLayout.CENTER);
-        detailPanel.revalidate();
-        detailPanel.repaint();
+        cardsGridPanel.loadCards(Collections.unmodifiableList(list), 
+            card -> detailPanel.showCard(card, detailWidth), cardsOwned);
     }
 
     private void setComponentsOpacity(final JButton backButton) {
         makeComponentsTransparent(this);
         backButton.setOpaque(true);
-    }
-
-    private void addFilterButton(final String label,
-                                 final Optional<CardType> filterType,
-                                 final Set<CardIdentifier> cardsOwned) {
-        final var btn = new JButton(label);
-        btn.setFont(responsiveFont(Font.BOLD, BTN_FONT_SIZE));
-        btn.setPreferredSize(new Dimension(btn.getPreferredSize().width, FILTER_BUTTON_HEIGHT));
-        btn.addActionListener(e -> loadCards(filterType, cardsOwned));
-        filterBar.add(btn);
-    }
-
-    private static Font responsiveFont(final int style, final int size) {
-        final double scale = (double) SCREEN_WIDTH / BASE_SCREEN_WIDTH;
-        return new Font(FONT_NAME, style, (int) (size * scale));
     }
 }
