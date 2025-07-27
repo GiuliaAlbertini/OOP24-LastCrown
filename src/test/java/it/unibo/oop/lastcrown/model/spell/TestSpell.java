bcaf57e713041e5c21c921cbff85beb94d9980e6
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-07-27 21:38:09 +0200

    Change TestSpell in order to follow the last commits of branch feature_spell

--- src/test/java/it/unibo/oop/lastcrown/model/spell/TestSpell.java
+++ src/test/java/it/unibo/oop/lastcrown/model/spell/TestSpell.java
@@ -2,11 +2,10 @@ package it.unibo.oop.lastcrown.model.spell;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 
 import it.unibo.oop.lastcrown.model.spell.api.SpellEffect;
+import it.unibo.oop.lastcrown.model.card.CardType;
 import it.unibo.oop.lastcrown.model.spell.api.Spell;
 import it.unibo.oop.lastcrown.model.spell.impl.SpellFactory;
 import it.unibo.oop.lastcrown.model.spell.impl.SpellImpl;
@@ -21,16 +20,13 @@ final class TestSpell {
     private static final int ENERGY_TO_PLAY = 4;
     private static final String CATEGORY = "health";
     private static final int AMOUNT = 40;
-    private static final String TARGET = "enemies";
-    private static final int ACTION_RANGE = 450;
+    private static final CardType TARGET = CardType.ENEMY;
 
     private final Spell spell1 = new SpellImpl(NAME, COST, COPIES_PER_ROUND,
-    ENERGY_TO_PLAY, new SpellEffect(CATEGORY, AMOUNT, TARGET, Optional.empty()),
-    Optional.of(ACTION_RANGE));
+    ENERGY_TO_PLAY, new SpellEffect(CATEGORY, AMOUNT, TARGET));
 
     private final Spell spell2 = SpellFactory.createSpell(NAME, COST, COPIES_PER_ROUND,
-    ENERGY_TO_PLAY, new SpellEffect(CATEGORY, AMOUNT, TARGET, Optional.empty()),
-    Optional.of(ACTION_RANGE));
+    ENERGY_TO_PLAY, new SpellEffect(CATEGORY, AMOUNT, TARGET));
 
     /**
      * Test the getters of SpellImpl.
@@ -41,8 +37,7 @@ final class TestSpell {
         assertEquals(COST, spell1.getCost());
         assertEquals(COPIES_PER_ROUND, spell1.getCopiesPerMatch());
         assertEquals(ENERGY_TO_PLAY, spell1.getEnergyToPlay());
-        assertEquals(new SpellEffect(CATEGORY, AMOUNT, TARGET, Optional.empty()), spell1.getSpellEffect());
-        assertEquals(Optional.of(ACTION_RANGE), spell1.getActionRange());
+        assertEquals(new SpellEffect(CATEGORY, AMOUNT, TARGET), spell1.getSpellEffect());
     }
 
     /**
@@ -54,7 +49,6 @@ final class TestSpell {
         assertEquals(COST, spell2.getCost());
         assertEquals(COPIES_PER_ROUND, spell2.getCopiesPerMatch());
         assertEquals(ENERGY_TO_PLAY, spell2.getEnergyToPlay());
-        assertEquals(new SpellEffect(CATEGORY, AMOUNT, TARGET, Optional.empty()), spell2.getSpellEffect());
-        assertEquals(Optional.of(ACTION_RANGE), spell2.getActionRange());
+        assertEquals(new SpellEffect(CATEGORY, AMOUNT, TARGET), spell2.getSpellEffect());
     }
 }
