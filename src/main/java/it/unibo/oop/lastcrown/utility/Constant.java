514578cb0b3549fe502fc36f8ddb8fdadddf91fa
Giulia <giulia.albertini3@studio.unibo.it>
2025-08-06 16:47:03 +0200

    Add Constants class

---
+++ src/main/java/it/unibo/oop/lastcrown/utility/Constant.java
@@ -0,0 +1,15 @@
+package it.unibo.oop.lastcrown.utility;
+
+public class Constant {
+    private Constant() { }
+
+    public static final int HITBOX_WIDTH = 10;
+    public static final int HITBOX_HEIGHT = 10;
+
+    public static final int DEFAULT_MELEE_RADIUS = 250;
+    public static final int DEFAULT_RANGED_RADIUS = 475;
+    public static final int DEFAULT_BOSS_RADIUS = 400;
+
+    public static final int UPGRADE_RADIUS_MELEE = 400;
+    public static final int UPGRADE_RADIUS_RANGED = 800;
+}
