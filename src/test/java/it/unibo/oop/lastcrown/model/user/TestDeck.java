6c708a372c0cb825b45f8dfb0748a8ac2268bc2f
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-08-06 17:48:05 +0200

    Refactor in order to use the factory

--- src/test/java/it/unibo/oop/lastcrown/model/user/TestDeck.java
+++ src/test/java/it/unibo/oop/lastcrown/model/user/TestDeck.java
@@ -4,32 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Set;
 
-import org.junit.jupiter.api.BeforeEach; import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import it.unibo.oop.lastcrown.model.card.CardIdentifier; import it.unibo.oop.lastcrown.model.card.CardType;
+import it.unibo.oop.lastcrown.model.card.CardIdentifier;
+import it.unibo.oop.lastcrown.model.card.CardType;
 import it.unibo.oop.lastcrown.model.user.api.Deck;
-import it.unibo.oop.lastcrown.model.user.impl.DeckImpl;
+import it.unibo.oop.lastcrown.model.user.impl.StandardDeckFactory;
 
 final class DeckTest { 
     private Deck deck; 
     private CardIdentifier hero; 
     private CardIdentifier meleeCard;
+
     @BeforeEach
     void setUp() {
         hero = new CardIdentifier(1, CardType.HERO);
         meleeCard = new CardIdentifier(2, CardType.MELEE);
         final Set<CardIdentifier> collection = Set.of(hero, meleeCard);
-        deck = DeckImpl.createDeck(collection);
-    }
-
-    @Test
-    void testInitHero() {
-        assertEquals(hero, deck.getHero());
-        assertTrue(deck.getDeck().contains(hero));
+        deck = StandardDeckFactory.createDeck(collection);
     }
 
     @Test
     void testAddAndRemoveCard() {
+        deck.addCard(hero);
+        assertTrue(deck.getDeck().contains(hero));
         deck.addCard(meleeCard);
         assertTrue(deck.getDeck().contains(meleeCard));
         deck.removeCard(meleeCard);
@@ -38,7 +37,7 @@ final class DeckTest {
 
     @Test
     void testCannotAddUnknownCard() {
-        final CardIdentifier unknown = new CardIdentifier(5, CardType.RANGED);
+        final CardIdentifier unknown = new CardIdentifier(25, CardType.RANGED);
         deck.addCard(unknown);
         assertFalse(deck.getDeck().contains(unknown));
     }
@@ -47,7 +46,8 @@ final class DeckTest {
     void testSwitchHero() {
         final CardIdentifier newHero = new CardIdentifier(3, CardType.HERO);
         final Set<CardIdentifier> col = Set.of(newHero);
-        final DeckImpl newDeck = (DeckImpl) DeckImpl.createDeck(col);
+        final Deck newDeck = StandardDeckFactory.createDeck(col);
+        newDeck.addCard(newHero);
         assertEquals(newHero, newDeck.getHero());
     }
 }
