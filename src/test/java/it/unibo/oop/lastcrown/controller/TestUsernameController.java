cc6c0fbdb99239030b9fb4c23ff8ce7a5ebcd512
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-08-08 17:10:28 +0200

    Change position of the TestUsernameController

--- src/test/java/it/unibo/oop/lastcrown/controller/TestUsernameController.java
+++ src/test/java/it/unibo/oop/lastcrown/controller/TestUsernameController.java
@@ -1,6 +1,7 @@
-package it.unibo.oop.lastcrown;
+package it.unibo.oop.lastcrown.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach; import org.junit.jupiter.api.Test;
 
